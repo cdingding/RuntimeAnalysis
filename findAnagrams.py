dd28b632a1ac9db71ea4ad1501affcd751069da8
Charlie <memsding@gmail.com>
2016-11-20 13:58:11 -0800

    review more

--- findAnagrams.py
+++ findAnagrams.py
@@ -3,6 +3,26 @@ import timeit
 from collections import Counter, defaultdict
 from itertools import permutations
 
+def find_anagrams1(lst):
+    result = []
+    for i, word1 in enumerate(lst):
+        for j, word2 in enumerate(lst[i+1:]):
+            if sorted(word1) == sorted(word2) and word1 not in result:
+                result.append(word1)
+            if sorted(word1) == sorted(word2) and word2 not in result:
+                result.append(word2)
+                break
+    return result
+
+def find_anagrams2(lst):
+    result = []
+    for i, word1 in enumerate(lst):
+        for j, word2 in enumerate(lst[i+1:]):
+            if sorted(word1) == sorted(word2):
+                result.append(word1)
+                result.append(word2)
+                break
+    return list(set(result))
 
 def find_anagrams_permu(lst):
     results = []
@@ -26,7 +46,7 @@ def find_anagrams_sortbrk(lst):
     results = []
     for word1 in lst:
         for word2 in lst:
-            if word1!=word2 and sorted(word1)==sorted(word2):
+            if word1 != word2 and sorted(word1) == sorted(word2):
                 results.append(word1)
                 break
     return results
@@ -61,6 +81,28 @@ def cal_time(func, lst):
     print 'For timeit, %s run time is: %f ms.' %(func.__name__, (timeit.default_timer() - start_time)*1000)
     print func(lst)
 
+def findit1(lst): #diversity to highly ordered
+    d = {}
+    result = []
+    for word in lst: #setup data structure, find by conition using available resourses such as index, zip/tuple pairs
+        if tuple(sorted(word)) not in d:
+            d[tuple(sorted(word))] = []
+        d[tuple(sorted(word))].append(word)
+    for key, value in d.iteritems():
+        if len(value) > 1:
+            result.extend(value)
+    return result
+
+def findit(lst):
+    d = defaultdict(list)
+    result = []
+    for word in lst:
+        d[tuple(sorted(word))].append(word)
+    for key, value in d.iteritems():
+        if len(value) > 1:
+            result.extend(value)
+    return result
+
 if __name__=='__main__':
     lst = ['bat', 'rats', 'god', 'dog', 'cat', 'arts', 'star']
     cal_time(find_anagrams_counter,lst)
@@ -68,3 +110,7 @@ if __name__=='__main__':
     cal_time(find_anagrams_notin, lst)
     cal_time(find_anagrams_sortbrk,lst)
     cal_time(find_anagrams_dct,lst)
+    cal_time(find_anagrams1, lst)
+    cal_time(find_anagrams2, lst)
+    print findit(lst)
+    print findit1(lst)
